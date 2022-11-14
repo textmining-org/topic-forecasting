3911c19713abad2ed4231dd02ad465cfa9cb79c9
sklim84 <captiong84@gmail.com>
2022-11-15 01:06:35 +0900

    test GCN for patents

--- GCNs/AGCRN_test.py
+++ GCNs/AGCRN_test.py
@@ -9,18 +9,18 @@ from torch_geometric_temporal.signal import DynamicGraphTemporalSignal
 from torch_geometric_temporal.signal import temporal_signal_split
 from tqdm import tqdm
 
-from preprocessed.utils import get_node_targets, get_edge_indices_and_weights
+from preprocessed.utils import get_node_targets, get_node_features, get_edge_indices, get_edge_weights
 
 
 class RecurrentGCN(torch.nn.Module):
-    def __init__(self, number_of_nodes, node_features):
+    def __init__(self, number_of_nodes, in_channels, out_channels, embedding_dimensions):
         super(RecurrentGCN, self).__init__()
         self.recurrent = AGCRN(number_of_nodes=number_of_nodes,
-                               in_channels=node_features,
-                               out_channels=2,
+                               in_channels=in_channels,
+                               out_channels=out_channels,
                                K=2,
-                               embedding_dimensions=4)
-        self.linear = torch.nn.Linear(2, 1)
+                               embedding_dimensions=embedding_dimensions)
+        self.linear = torch.nn.Linear(out_channels, 1)
 
     def forward(self, x, e, h):
         h_0 = self.recurrent(x, e, h)
@@ -40,41 +40,41 @@ class RecurrentGCN(torch.nn.Module):
 # - edge weights : 36개 (시점 0, co-occurrence)
 #########################
 
-node_targets = get_node_targets()
+media = 'patents'
+topic_num = 1
+embedding_dimensions = 4
+out_channels = 2
+
+# node targets(label)
+node_targets = get_node_targets(media, topic_num)
 print('node targets: {}'.format(node_targets.shape))
 
 number_of_nodes = node_targets[0].shape[0]
 number_of_features = 1
 
-# FIXME 추후 centrality 등으로 변경 필요
-node_features = np.random.rand(18, number_of_nodes, number_of_features)
+# node features
+node_features = get_node_features(media, topic_num)
+# node_features = np.random.rand(18, number_of_nodes, number_of_features)
 print('node feature: {}'.format(node_features.shape))
 
-dict_edge_indices, dict_edge_weights = get_edge_indices_and_weights()
-with open('./preprocessed/edge_attributes.txt', 'r') as f_eattr:
-    edge_attrs = f_eattr.read().splitlines()
+# edge indices and weights
+edge_indices = get_edge_indices(media, topic_num)
+edge_weights = get_edge_weights(media, topic_num)
 
-    edge_indices = []
-    edge_weights = []
-    for edge_attr in edge_attrs:
-        if edge_attr in dict_edge_indices and edge_attr in dict_edge_weights:
-            edge_indices.append(np.array(dict_edge_indices[edge_attr]))
-            edge_weights.append(np.array(dict_edge_weights[edge_attr]))
-
-    print('edge indices: {} * {}'.format(len(edge_indices), edge_indices[0].shape))
-    print('edge weights: {} * {}'.format(len(edge_weights), edge_weights[0].shape))
-
-    dataset = DynamicGraphTemporalSignal(edge_indices, edge_weights, node_features, node_targets)
+dataset = DynamicGraphTemporalSignal(edge_indices, edge_weights, node_features, node_targets)
 
 #########################
 # 2. Model training
 #########################
 train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
-model = RecurrentGCN(number_of_nodes=number_of_nodes, node_features=number_of_features)
+model = RecurrentGCN(number_of_nodes=number_of_nodes,
+                     in_channels=number_of_features,
+                     out_channels=out_channels,
+                     embedding_dimensions=embedding_dimensions)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 model.train()
-e = torch.empty(number_of_nodes, 4)
+e = torch.empty(number_of_nodes, embedding_dimensions)
 torch.nn.init.xavier_uniform_(e)
 for epoch in tqdm(range(200)):
     cost = 0
--- GCNs/preprocessed/utils.py
+++ GCNs/preprocessed/utils.py
@@ -1,208 +1,84 @@
-import os
-import sys
-
-parent_path = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
-sys.path.append(parent_path)
-
-import networkx as nx
-import numpy as np
-import pandas as pd
-import logging
 import json
+import os
 import pathlib
 
-from network_analysis.graph_reconstruction import load_graph
-from torch_geometric_temporal.signal import DynamicGraphTemporalSignal, StaticGraphTemporalSignal
+import numpy as np
 
 
-def get_node_indices():
+def get_node_targets(media, topic_num):
     loc_here = pathlib.Path(__file__).resolve().parent
 
-    loc_node_indices = os.path.join(loc_here, 'node_indices.tsv')
-    if os.path.exists(loc_node_indices):
-        return pd.read_csv(loc_node_indices, sep='\t', names=['node_index', 'node_name'])
-
-    loc_graph = os.path.join(loc_here, 'combined_graph.pkl')
-    assert os.path.exists(loc_graph), "There does not exist graph file(named 'combined graph.pkl)"
-
-    G = load_graph(loc_graph)
-    df_node_indices = pd.DataFrame({'node_index': '', 'node_name': G.nodes})
-    df_node_indices['node_index'] = df_node_indices.index
-    df_node_indices.to_csv(loc_node_indices, sep='\t', header=False, index=False)
-
-    return df_node_indices
-
-
-def get_node_targets():
-    loc_here = pathlib.Path(__file__).resolve().parent
-
-    loc_node_targets = os.path.join(loc_here, 'node_targets.npy')
+    loc_node_targets = os.path.join(loc_here / media / '4.topic' / str(topic_num), 'word_count.node_targets.npy')
     if os.path.exists(loc_node_targets):
         return np.load(loc_node_targets)
 
-    loc_graph = os.path.join(loc_here, 'combined_graph.pkl')
-    assert os.path.exists(loc_graph), "There does not exist graph file(named 'combined graph.pkl)"
-
-    loc_node_indices = os.path.join(loc_here, 'node_indices.tsv')
-    assert os.path.exists(loc_node_indices), "There does not exist node indices file(named 'node_indices.tsv')"
-
-    loc_node_attr = os.path.join(loc_here, 'node_attributes.txt')
-    assert os.path.exists(loc_node_attr), "There does not exist node attributes file(named 'node_attributes.txt')"
-
-    G = load_graph(loc_graph)
-    node_indices = pd.read_csv(loc_node_indices, sep='\t', names=['node_index', 'node_name'])
-    node_names = node_indices['node_name'].tolist()
-
-    with open(loc_node_attr, 'r') as f_nattr:
-        node_attrs = f_nattr.read().splitlines()
-        targets = []
-
-        for node_attr in node_attrs:
-            target = []
-            for node_name in node_names:
-                node_attr = node_attr.strip()
-                target.append(G.nodes[node_name][node_attr])
-            targets.append(np.asarray(target))
-        f_nattr.close()
-        np.save(loc_node_targets, targets)
-
-    return np.load(loc_node_targets)
+    raise Exception(f'There is no node targets! (media: {media}, topic num: {topic_num})')
 
 
-def get_edge_list():
+def get_edge_indices(media, topic_num):
     loc_here = pathlib.Path(__file__).resolve().parent
 
-    loc_edge_list = os.path.join(loc_here, 'edge_list.tsv')
-    if os.path.exists(loc_edge_list):
-        return pd.read_csv('edge_list.tsv', sep='\t', names=['u', 'v'])
+    loc_edge_indices = os.path.join(loc_here / media / '4.topic' / str(topic_num), 'cooccurrence.edge_indices.json')
+    if os.path.exists(loc_edge_indices):
+        with open(os.path.join(loc_here / media / '4.topic' / str(topic_num), 'cooccurrence.edge_indices.json'),
+                  'r') as f:
+            dict_edge_indices = json.load(f)
+            with open(f'./preprocessed/{media}/4.topic/{str(topic_num)}/cooccurrence.edge_attributes.txt',
+                      'r') as f_eattr:
+                edge_attrs = f_eattr.read().splitlines()
+                edge_indices = []
+                for edge_attr in edge_attrs:
+                    if edge_attr in dict_edge_indices:
+                        edge_index = np.array(dict_edge_indices[edge_attr])
+                        if edge_index.size != 0:
+                            src_index = np.split(edge_index[0], 2)[0]
+                            dst_index = np.split(edge_index[1], 2)[0]
+                            edge_index = np.vstack([src_index, dst_index])
+                        edge_indices.append(edge_index)
 
-    loc_graph = os.path.join(loc_here, 'combined_graph.pkl')
-    assert os.path.exists(loc_graph), "There does not exist graph file(named 'combined graph.pkl)"
-    G = load_graph(loc_graph)
+                print('edge indices: {} * {}'.format(len(edge_indices), edge_indices[0].shape))
 
-    df_edges = pd.DataFrame(G.edges, columns=['u', 'v'])
-    df_edges.to_csv(loc_edge_list, sep='\t', header=False, index=False)
+            return edge_indices
 
-    return df_edges
+    raise Exception(f'There is no edge indices! (media: {media}, topic num: {topic_num})')
 
 
-def get_edge_indices_and_weights():
-    # logging.basicConfig(filename='get_edge_weights.log', level=logging.DEBUG)
-
+def get_edge_weights(media, topic_num):
     loc_here = pathlib.Path(__file__).resolve().parent
 
-    loc_edge_indices = os.path.join(loc_here, 'edge_indices.json')
-    loc_edge_weights = os.path.join(loc_here, 'edge_weights.json')
-    if os.path.exists(loc_edge_indices) and os.path.exists(loc_edge_weights):
-        with open(os.path.join(loc_here, 'edge_indices.json'), 'r') as f_i, \
-                open(os.path.join(loc_here, 'edge_weights.json'), 'r') as f_w:
-            edge_indices = json.load(f_i)
-            edge_weights = json.load(f_w)
+    loc_edge_weights = os.path.join(loc_here / media / '4.topic' / str(topic_num), 'cooccurrence.edge_weigths.json')
+    if os.path.exists(loc_edge_weights):
+        with open(os.path.join(loc_here / media / '4.topic' / str(topic_num), 'cooccurrence.edge_weigths.json'),
+                  'r') as f:
+            dict_edge_weights = json.load(f)
+            with open(f'./preprocessed/{media}/4.topic/{str(topic_num)}/cooccurrence.edge_attributes.txt',
+                      'r') as f_eattr:
+                edge_attrs = f_eattr.read().splitlines()
+                edge_weights = []
+                for edge_attr in edge_attrs:
+                    if edge_attr in dict_edge_weights:
+                        edge_weight = np.array(dict_edge_weights[edge_attr])
+                        if edge_weight.size != 0:
+                            edge_weight = np.split(edge_weight, 2)[0]
+                        edge_weights.append(edge_weight)
 
-            return edge_indices, edge_weights
+                print('edge weights: {} * {}'.format(len(edge_weights), edge_weights[0].shape))
 
-    loc_graph = os.path.join(loc_here, 'combined_graph.pkl')
-    assert os.path.exists(loc_graph), "There does not exist graph file(named 'combined graph.pkl)"
+            return edge_weights
 
-    loc_node_indices = os.path.join(loc_here, 'node_indices.tsv')
-    assert os.path.exists(loc_node_indices), "There does not exist node indices file(named 'node_indices.tsv')"
+    raise Exception(f'There is no edge weights! (media: {media}, topic num: {topic_num})')
 
-    loc_edge_list = os.path.join(loc_here, 'edge_list.tsv')
-    assert os.path.exists(loc_edge_list), "There does not exist edge list file(named 'edge_list.tsv')"
 
-    loc_edge_attr = os.path.join(loc_here, 'edge_attributes.txt')
-    assert os.path.exists(loc_edge_attr), "There does not exist edge attributes file(named 'edge_attributes.txt')"
-
-    G = load_graph(loc_graph)
-    node_indices = pd.read_csv(loc_node_indices, sep='\t', names=['node_index', 'node_name'])
-    edge_list = pd.read_csv(loc_edge_list, sep='\t', names=['u', 'v'])
-
-    with open(loc_edge_attr, 'r') as f_eattr:
-        edge_attrs = f_eattr.read().splitlines()
-        dict_edge_indices = {}
-        dict_edge_weights = {}
-
-        for edge_attr in edge_attrs:
-            node_u_indices = []
-            node_v_indices = []
-            edge_weights = []
-
-            for i, edge in edge_list.iterrows():
-                edge_data = G.get_edge_data(u=edge['u'], v=edge['v'], default=0)
-                edge_weight = edge_data.get(edge_attr, 0)
-
-                if i % 10000 == 0:
-                    # logging.debug('{} {} {} {}'.format(i, edge.values, edge_attr, edge_weight))
-                    print('{} {} {} {}'.format(i, edge.values, edge_attr, edge_weight))
-
-                if edge_weight != 0:
-                    node_u_index = node_indices[node_indices['node_name'] == edge['u']]['node_index'].values[0]
-                    node_v_index = node_indices[node_indices['node_name'] == edge['v']]['node_index'].values[0]
-                    node_u_indices.append(node_u_index)
-                    node_v_indices.append(node_v_index)
-
-                    edge_weights.append(edge_weight)
-
-            if len(node_u_indices) != 0 and len(node_v_indices) != 0:
-                dict_edge_indices[edge_attr] = np.stack((node_u_indices, node_v_indices), axis=0).tolist()
-
-            if len(edge_weights) != 0:
-                dict_edge_weights[edge_attr] = edge_weights
-
-        # FIXME return JSON → return numpy
-        # edge_indices = []
-        # edge_weights = []
-        # for edge_attr in edge_attrs:
-        #     edge_indices.append(np.array(dict_edge_indices[edge_attr]))
-        #     edge_weights.append(np.array(dict_edge_weights[edge_attr]))
-        #
-        # np.save(loc_edge_indices, edge_indices)
-        # np.save(loc_edge_weights, edge_weights)
-
-        with open(loc_edge_indices, 'w') as f_i, open(loc_edge_weights, 'w') as f_w:
-            json.dump(dict_edge_indices, f_i)
-            json.dump(dict_edge_weights, f_w)
-
-    return dict_edge_indices, dict_edge_indices
-
-def get_node_features():
-    # TODO implements calculate centrality for nodes
-
+def get_node_features(media, topic_num):
     loc_here = pathlib.Path(__file__).resolve().parent
-    loc_graph = os.path.join(loc_here, 'combined_graph.pkl')
-    G = load_graph(loc_graph)
 
-    ev_centrality = nx.eigenvector_centrality_numpy(G)
+    loc_node_features = os.path.join(loc_here / media / '4.topic' / str(topic_num),
+                                     'betweenness_centrality.inv_cooccurrence.node_targets.npy')
+    if os.path.exists(loc_node_features):
+        node_features = np.load(loc_node_features)
+        node_features = np.expand_dims(node_features, axis=2)
+        # FIXME
+        node_features = np.nan_to_num(node_features)
+        return node_features
 
-    # for node, centrality in ev_centrality.items():
-    # bw_centrality = nx.betweenness_centrality(G)
-    # cl_centrality = nx.closeness_centrality(G)
-
-    # print(ev_centrality)
-    # print(bw_centrality)
-    # print(cl_centrality)
-
-
-if __name__ == "__main__":
-
-    get_node_indices()
-    get_node_targets()
-    get_edge_list()
-
-    dict_edge_indices, dict_edge_weights = get_edge_indices_and_weights()
-    output_dir = os.path.abspath('./')
-    with open(os.path.join(output_dir, 'edge_attributes.txt'), 'r') as f_eattr:
-        edge_attrs = f_eattr.read().splitlines()
-
-        edge_indices = []
-        edge_weights = []
-        for edge_attr in edge_attrs:
-            if edge_attr in dict_edge_indices and edge_attr in dict_edge_weights:
-                edge_indices.append(np.array(dict_edge_indices[edge_attr]))
-                edge_weights.append(np.array(dict_edge_weights[edge_attr]))
-
-        print('edge indices length: {}'.format(len(edge_indices)))
-        print('edge indices[0] shape: {}'.format(edge_indices[1].shape))
-
-        print('edge weights length: {}'.format(len(edge_weights)))
-        print('edge weights[0] shape: {}'.format(edge_weights[1].shape))
+    raise Exception(f'There is no node features! (media: {media}, topic num: {topic_num})')
