0b77fb60558fd86eeee96c7f20ddf50089c661af
sklim84 <captiong84@gmail.com>
2023-04-11 17:07:43 +0900

    코드 정리

--- GCNs/run_vis.sh
+++ GCNs/run_vis.sh
@@ -4,7 +4,7 @@ num_training_clusters=100
 for node_feature_type in "betweenness closeness" "betweenness degree" "closeness degree" "betweenness closeness degree" "betweenness" "degree" "closeness"; do
   for model in dcrnn tgcn agcrn a3tgcn; do # dcrnn tgcn agcrn a3tgcn
     for device in 0; do
-      python3 -u main_fcst.py --seed $seed --model $model --node_feature_type $node_feature_type --device $device --num_training_clusters $num_training_clusters
+      python3 -u main_vis.py --seed $seed --model $model --node_feature_type $node_feature_type --device $device --num_training_clusters $num_training_clusters
     done
   done
 done
