e06f21ba9bfe4f536b64518989151e0d294c86d3
YEJPark <109940982+YEJPark@users.noreply.github.com>
2024-05-02 15:04:39 +0900

    Update opt_num_topics.py

--- topic_modeling/opt_num_topics.py
+++ topic_modeling/opt_num_topics.py
@@ -1,16 +1,14 @@
 import os
-
 import pandas as pd
 import plotly.graph_objects as go
 import tomotopy as tp
 from plotly.subplots import make_subplots
+import argparse
 
 from commons import exec_lda_modeling, exec_dmr_modeling
-import argparse
 
 def make_model(datasets_path, target_name, method_name, num_topics_range):
     preprocessed_path = os.path.join(datasets_path, target_name + '.pkl')
-
     df_datasets = pd.read_pickle(preprocessed_path)
     timestamps = df_datasets['date'].astype(str).values.tolist()
     documents = df_datasets['text'].values.tolist()
@@ -25,37 +23,32 @@ def make_model(datasets_path, target_name, method_name, num_topics_range):
         print(f'##### number of topics: {num_topics}')
         model.save(f'./opt_num_topics/{target_name}_{method_name}_{num_topics}.bin', full=True)
 
-
-def calc_coehrence_perplexity(target_name, method_name, num_topics_range):
+def calc_coherence_perplexity(target_name, method_name, num_topics_range):
     num_topics_list = list(num_topics_range)
-    # perplexity/coherence 계산
-    coherence_metric = 'c_v'  # u_mass(0에 가까울수록 일관성 높음), c_uci, c_npmi, c_v(0과1사이, 0.55정도 수준)
-    perplexities = []
-    coherences = []
+    coherence_metric = 'c_v'
+    data_list = []
     for num_topics in num_topics_list:
-        model_save_path = os.path.join('./opt_num_topics', target_name + '_' + method_name + '_' + str(num_topics) + '.bin')
+        model_save_path = os.path.join('./opt_num_topics', f'{target_name}_{method_name}_{num_topics}.bin')
         if method_name == 'lda':
             model = tp.LDAModel.load(model_save_path)
         elif method_name == 'dmr':
             model = tp.DMRModel.load(model_save_path)
         coherence = tp.coherence.Coherence(model, coherence=coherence_metric)
-        print(
-            'num topics: {}\tperplexity: {}\tcoherence: {}'.format(num_topics, model.perplexity, coherence.get_score()))
-        perplexities.append(model.perplexity)
-        coherences.append(coherence.get_score())
+        print(f'num topics: {num_topics}\tperplexity: {model.perplexity}\tcoherence: {coherence.get_score()}')
+        data_list.append({
+            'topic number': num_topics,
+            'perplexity': model.perplexity,
+            'coherence': coherence.get_score(),
+            'metric': coherence_metric
+        })
 
-    # csv 저장
-    columns = ['topic number', 'perplexity', 'coherence', 'metric']
-    df_result = pd.DataFrame(columns=columns)
-    df_result = df_result.append(
-        pd.DataFrame((zip(num_topics_list, perplexities, coherences, [coherence_metric] * len(num_topics_list))),
-                     columns=columns), ignore_index=True)
-    print(df_result)
+    # Convert the list of dictionaries to a DataFrame
+    df_result = pd.DataFrame(data_list)
 
     result_file = f'./opt_num_topics/{target_name}_{method_name}_perplexity_coherence.csv'
     df_result.to_csv(result_file, index=False, encoding='utf-8-sig')
 
-    # plot perplexity/coherence
+    # Plot perplexity/coherence
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     trace1 = go.Scatter(x=df_result['topic number'], y=df_result['perplexity'], name='Perplexity')
     trace2 = go.Scatter(x=df_result['topic number'], y=df_result['coherence'], name='Coherence')
@@ -67,18 +60,16 @@ def calc_coehrence_perplexity(target_name, method_name, num_topics_range):
     fig.update_xaxes(title_text='Number of topics')
     fig.write_html(os.path.splitext(result_file)[0] + '.html')
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--target_name', default='papers')
-parser.add_argument('--method_name', default='dmr')
-args = parser.parse_args()
-
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target_name', default='papers')
+    parser.add_argument('--method_name', default='dmr')
+    args = parser.parse_args()
+
     datasets_path = os.path.abspath('../_datasets')
     print(datasets_path)
 
-    num_topics_range = range(2, 101, 2)
+    num_topics_range = range(2, 51, 2)
 
     make_model(datasets_path, args.target_name, args.method_name, num_topics_range)
-    calc_coehrence_perplexity(args.target_name, args.method_name, num_topics_range)
+    calc_coherence_perplexity(args.target_name, args.method_name, num_topics_range)
