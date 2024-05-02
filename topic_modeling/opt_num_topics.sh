c7f6e6c5699143b4a71337c474d92fa7349a7bd2
YEJPark <109940982+YEJPark@users.noreply.github.com>
2024-05-02 15:05:32 +0900

    Update opt_num_topics.sh

--- topic_modeling/opt_num_topics.sh
+++ topic_modeling/opt_num_topics.sh
@@ -1,5 +1,5 @@
-for method_name in dmr; do
-  for target_name in patents papers news; do
+for method_name in dmr lda; do
+  for target_name in patent_201701_202312_without_duplicates paper_201701_202312_without_duplicates news_201701_202312_without_duplicates; do
     python3 -u opt_num_topics.py --method_name $method_name --target_name $target_name
   done
 done
